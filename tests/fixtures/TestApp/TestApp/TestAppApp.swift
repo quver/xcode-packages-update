2841f909b8906a0faf7b97a5500f9def757097ab
Paweł Bednorz <pawel@quver.pl>
2026-03-05 00:30:05 +0100

    ci: integration tests

---
+++ tests/fixtures/TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Pawel Bednorz on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
